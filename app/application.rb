c7c02e694c4aee7a44592e48a45beef6d467f627
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-23 12:56:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,17 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = Kernel.rand(1..2)
+    num_1 = Kernel.rand(1..5)
 
-    if num_1.even?
+    if num_1 == 1
       resp.write "Yes"
-    else
+    elsif num_1 == 2
+      resp.write "Outlook is good"
+    elsif num_1 == 3
+      resp.write "Signs point to no"
+    elsif num_1 == 4
+      resp.write "Try again"
+    elsif num_1 == 5
       resp.write "No"
     end
 
