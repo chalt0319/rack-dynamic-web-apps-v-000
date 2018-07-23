3fcea4f8c080d3eb501cc2b6dee5c861ffcb99c6
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-23 12:54:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,18 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    num_1 = Kernel.rand(1..3)
-    num_2 = Kernel.rand(1..3)
-    num_3 = Kernel.rand(1..3)
+    num_1 = Kernel.rand(1..2)
 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
-    if num_1 == num_2 && num_2 == num_3
-      resp.write "You Win"
+    if num_1.even?
+      resp.write "Yes"
     else
-      resp.write "You Lose"
+      resp.write "No"
     end
 
     resp.finish
